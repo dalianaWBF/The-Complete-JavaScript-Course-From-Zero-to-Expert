d4ec48f930cc528420711103ccadde28311ebb32
dalianaWBF <daliana@workersbenefitfund.com>
2023-06-14 07:39:55 -0500

    Challenge 01

--- my_code/Challenges/06-Working-With-Arrays/01/script.js
+++ my_code/Challenges/06-Working-With-Arrays/01/script.js
@@ -9,14 +9,40 @@ A dog is an adult if it is at least 3 years old, and it's a puppy if it's less t
 Your tasks:
 Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
     1. Julia found out that the owners of the first and the last two dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
-    2. Create an array with both Julia's(corrected)andKate'sdata
+    2. Create an array with both Julia's(corrected)and Kate's data
     3. For each remaining dog,log to the console whether it's an adult ("Dog number1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy")
     4. Run the function for both test data sets
 
 Test data:
-    - Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3] 
-    - Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+    - Data 1: Julia's data [3, 5, 2, 12, 7], 
+              Kate's data [4, 1, 15, 8, 3] 
+    - Data 2: Julia's data [9, 16, 6, 8, 3], 
+              Kate's data [10, 5, 6, 1, 4]
     
 Hints: Use tools from all lectures in this section so far 😉
 
 */
+
+//Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'):
+const juliaData = [3, 5, 2, 12, 7];
+const kateData = [4, 1, 15, 8, 3];
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  //1. Create a shallow copy of Julia's array, and remove the cat ages from that copied array
+  const dogsJuliaCopy = dogsJulia.slice(1, -2);
+  console.log(dogsJuliaCopy);
+
+  //2. Create an array with both Julia's(corrected)and Kate's data
+  const juliaAndKateArray = dogsJuliaCopy.concat(dogsKate);
+  console.log(juliaAndKateArray);
+
+  //3. For each remaining dog,log to the console whether it's an adult
+  juliaAndKateArray.forEach((dog, index, arr) => {
+    dog >= 3
+      ? console.log(`Dog number ${index+1} is an adult, and is ${dog} years old`)
+      : console.log(`Dog number ${index+1} is still a puppy 🐶`);
+  });
+};
+
+checkDogs(juliaData, kateData);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
--- my_code/Challenges/06-Working-With-Arrays/03/script.js
+++ my_code/Challenges/06-Working-With-Arrays/03/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+/*
+Coding Challenge #3
+
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time as an arrow function, and using chaining!
+
+Test data:
+    - Data1:[5,2,4,1,15,8,3] 
+    - Data2:[16,6,10,5,6,1,4]
+*/
